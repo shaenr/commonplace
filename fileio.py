cf82489de3e75b7bd694a6f303d147d84ec98974
shaenr <mockidlat@protonmail.com>
2022-02-11 01:28:28 -0600

    refactoring names before it's too late

--- fileio.py
+++ fileio.py
@@ -1,15 +1,12 @@
 import json
 from pathlib import Path
 
-def cache_data_to_json(data: dict,
-                       path_obj: Path,
-                       mode: str = 'w'):
+
+def serialize_json_file(data: dict, path_obj: Path, mode: str = "w"):
     with path_obj.open(mode) as write_obj:
         json.dump(data, write_obj, indent=4)
 
 
-def deserialize_from_json(path_obj: Path,
-                          mode: str = 'r'):
+def deserialize_json_file(path_obj: Path, mode: str = "r"):
     with path_obj.open(mode) as read_file:
         return json.load(read_file)
-
--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='commonplace',
-      version='0.1',
-      description='A package of familiar utilities that are so reoccurring, they are in their own portable package and are commonplace.',
-      author='shaenr',
-      author_email='virufac@protonmail.com',
-      url='https://github.com/shaenr/commonplace',
-      packages=[],
-     )
+setup(
+    name="commonplace",
+    version="0.1",
+    description="A package of familiar utilities that are so reoccurring, this package is commonplace.",
+    author="shaenr",
+    author_email="virufac@protonmail.com",
+    url="https://github.com/shaenr/commonplace",
+    packages=[],
+)

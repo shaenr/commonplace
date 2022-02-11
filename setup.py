19481a66d2d5f5f1fb62b5dd9a8a2d959f82cb2c
shaenr <mockidlat@protonmail.com>
2022-02-10 18:16:28 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='commonplace',
+      version='0.1',
+      description='A package of familiar utilities that are so reoccurring, they are in their own portable package and are commonplace.',
+      author='shaenr',
+      author_email='virufac@protonmail.com',
+      url='https://github.com/shaenr/commonplace',
+      packages=[],
+     )
